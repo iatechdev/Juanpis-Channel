841660627af6e6de6c6d57049770366c4c894559
iatechdev <Mauricio.roldan@iatech.com.co>
2020-08-20 21:07:11 -0500

    mejora

---
+++ js/index.js
@@ -0,0 +1,322 @@
+function displayvideo(){
+
+    document.getElementById("viewhovertwo").style.backgroundColor="#0e1c2c";
+    document.getElementById("viewhovertwo").style.height="450px";
+    document.getElementById("viewhoverthree").style.backgroundColor="";
+    document.getElementById("viewhoverthree").style.height="450px";
+    document.getElementById("view").style.display="block";
+    document.getElementById("viewhover").style.backgroundImage="url('../img/rp-hover.png')";
+    document.getElementById("viewhover").style.height="500px";
+
+}
+
+function displayvideonew(){
+
+    document.getElementById("viewhovertwonews").style.backgroundColor="#0e1c2c";
+    document.getElementById("viewhovertwonews").style.height="450px";
+    document.getElementById("viewhoverthreenews").style.backgroundColor="";
+    document.getElementById("viewhoverthreenews").style.height="450px";
+    document.getElementById("viewnews").style.display="block";
+    document.getElementById("viewhovernews").style.backgroundImage="url('../img/rp-hover.png')";
+    document.getElementById("viewhovernews").style.height="500px";
+
+}
+function displayvideofriends(){
+
+    document.getElementById("viewhovertwofriends").style.backgroundColor="#0e1c2c";
+    document.getElementById("viewhovertwofriends").style.height="450px";
+    document.getElementById("viewhoverthreefriends").style.backgroundColor="";
+    document.getElementById("viewhoverthreefriends").style.height="450px";
+    document.getElementById("viewfriends").style.display="block";
+    document.getElementById("viewhoverfriends").style.backgroundImage="url('../img/rp-hover.png')";
+    document.getElementById("viewhoverfriends").style.height="500px";
+
+}
+function displayvideogomelo(){
+    document.getElementById("viewgomelo").style.display="block";
+    document.getElementById("viewhovertwogomelo").style.backgroundColor="#0e1c2c";
+    document.getElementById("viewhovertwogomelo").style.height="450px";
+    document.getElementById("viewhoverthreegomelo").style.backgroundColor="";
+    document.getElementById("viewhoverthreegomelo").style.height="450px";
+    document.getElementById("viewhovergomelo").style.backgroundImage="url('../img/rp-hover.png')";
+    document.getElementById("viewhovergomelo").style.height="500px";
+  
+
+}
+function displayvideopoli(){
+    document.getElementById("viewpoli").style.display="block";
+    document.getElementById("viewhovertwopoli").style.backgroundColor="#0e1c2c";
+    document.getElementById("viewhovertwopoli").style.height="450px";
+    document.getElementById("viewhoverthreepoli").style.backgroundColor="";
+    document.getElementById("viewhoverthreepoli").style.height="450px";
+    document.getElementById("viewhoverpoli").style.backgroundImage="url('../img/rp-hover.png')";
+    document.getElementById("viewhoverpoli").style.height="500px";
+  
+
+}
+function displayvideopoli(){
+    document.getElementById("viewpoli").style.display="block";
+    document.getElementById("viewhovertwopoli").style.backgroundColor="#0e1c2c";
+    document.getElementById("viewhovertwopoli").style.height="450px";
+    document.getElementById("viewhoverthreepoli").style.backgroundColor="";
+    document.getElementById("viewhoverthreepoli").style.height="450px";
+    document.getElementById("viewhoverpoli").style.backgroundImage="url('../img/rp-hover.png')";
+    document.getElementById("viewhoverpoli").style.height="500px";
+  
+
+}
+function displayvideoheros(){
+    document.getElementById("viewheros").style.display="block";
+    document.getElementById("viewhovertwoheros").style.backgroundColor="#0e1c2c";
+    document.getElementById("viewhovertwoheros").style.height="450px";
+    document.getElementById("viewhoverthreeheros").style.backgroundColor="";
+    document.getElementById("viewhoverthreeheros").style.height="450px";
+    document.getElementById("viewhoverheros").style.backgroundImage="url('../img/rp-hover.png')";
+    document.getElementById("viewhoverheros").style.height="500px";
+  
+
+}
+function displayvideotwo(){
+    document.getElementById("viewhover").style.backgroundColor="";
+    document.getElementById("viewhover").style.height="450px";
+    document.getElementById("viewhoverthree").style.backgroundColor="";
+    document.getElementById("viewhoverthree").style.height="450px";
+    document.getElementById("view").style.display="block";
+    document.getElementById("viewhovertwo").style.backgroundImage="url('../img/rp-hover.png')";
+    document.getElementById("viewhovertwo").style.height="500px";
+
+}
+function displayvideotwonews(){
+
+    document.getElementById("viewhovernews").style.backgroundColor="#0e1c2c";
+    document.getElementById("viewhovernews").style.height="450px";
+    document.getElementById("viewhoverthreenews").style.backgroundColor="";
+    document.getElementById("viewhoverthreenews").style.height="450px";
+    document.getElementById("viewnews").style.display="block";
+    document.getElementById("viewhovertwonews").style.backgroundImage="url('../img/rp-hover.png')";
+    document.getElementById("viewhovertwonews").style.height="500px";
+
+}
+function displayvideotwopoli(){
+
+    document.getElementById("viewhoverpoli").style.backgroundColor="#0e1c2c";
+    document.getElementById("viewhoverpoli").style.height="450px";
+    document.getElementById("viewhoverthreepoli").style.backgroundColor="";
+    document.getElementById("viewhoverthreepoli").style.height="450px";
+    document.getElementById("viewpoli").style.display="block";
+    document.getElementById("viewhovertwopoli").style.backgroundImage="url('../img/rp-hover.png')";
+    document.getElementById("viewhovertwopoli").style.height="500px";
+
+}
+function displayvideotwogomelo(){
+
+    document.getElementById("viewhovergomelo").style.backgroundColor="#0e1c2c";
+    document.getElementById("viewhovergomelo").style.height="450px";
+    document.getElementById("viewhoverthreegomelo").style.backgroundColor="";
+    document.getElementById("viewhoverthreegomelo").style.height="450px";
+    document.getElementById("viewgomelo").style.display="block";
+    document.getElementById("viewhovertwogomelo").style.backgroundImage="url('../img/rp-hover.png')";
+    document.getElementById("viewhovertwogomelo").style.height="500px";
+
+}
+function displayvideotwofriends(){
+
+    document.getElementById("viewhoverfriends").style.backgroundColor="#0e1c2c";
+    document.getElementById("viewhoverfriends").style.height="450px";
+    document.getElementById("viewhoverthreefriends").style.backgroundColor="";
+    document.getElementById("viewhoverthreefriends").style.height="450px";
+    document.getElementById("viewfriends").style.display="block";
+    document.getElementById("viewhovertwofriends").style.backgroundImage="url('../img/rp-hover.png')";
+    document.getElementById("viewhovertwofriends").style.height="500px";
+
+}
+function displayvideotwoheros(){
+
+    document.getElementById("viewhoverheros").style.backgroundColor="#0e1c2c";
+    document.getElementById("viewhoverheros").style.height="450px";
+    document.getElementById("viewhoverthreeheros").style.backgroundColor="";
+    document.getElementById("viewhoverthreeheros").style.height="450px";
+    document.getElementById("viewheros").style.display="block";
+    document.getElementById("viewhovertwoheros").style.backgroundImage="url('../img/rp-hover.png')";
+    document.getElementById("viewhovertwoheros").style.height="500px";
+
+}
+function displayvideothree(){
+    document.getElementById("viewhover").style.backgroundColor="";
+    document.getElementById("viewhover").style.height="450px";
+    document.getElementById("viewhovertwo").style.backgroundColor="";
+    document.getElementById("viewhovertwo").style.height="450px";
+    document.getElementById("view").style.display="block";
+    document.getElementById("viewhoverthree").style.backgroundImage="url('../img/rp-hover.png')";
+    document.getElementById("viewhoverthree").style.height="500px";
+
+}
+function displayvideothreenews(){
+    document.getElementById("viewhovernews").style.backgroundColor="";
+    document.getElementById("viewhovernews").style.height="450px";
+    document.getElementById("viewhovertwonews").style.backgroundColor="";
+    document.getElementById("viewhovertwonews").style.height="450px";
+    document.getElementById("viewnews").style.display="block";
+    document.getElementById("viewhoverthreenews").style.backgroundImage="url('../img/rp-hover.png')";
+    document.getElementById("viewhoverthreenews").style.height="500px";
+
+}
+function displayvideothreegomelo(){
+    document.getElementById("viewhovergomelo").style.backgroundColor="";
+    document.getElementById("viewhovergomelo").style.height="450px";
+    document.getElementById("viewhovertwogomelo").style.backgroundColor="";
+    document.getElementById("viewhovertwogomelo").style.height="450px";
+    document.getElementById("viewgomelo").style.display="block";
+    document.getElementById("viewhoverthreegomelo").style.backgroundImage="url('../img/rp-hover.png')";
+    document.getElementById("viewhoverthreegomelo").style.height="500px";
+
+}
+function displayvideothreepoli(){
+    document.getElementById("viewhoverpoli").style.backgroundColor="";
+    document.getElementById("viewhoverpoli").style.height="450px";
+    document.getElementById("viewhovertwopoli").style.backgroundColor="";
+    document.getElementById("viewhovertwopoli").style.height="450px";
+    document.getElementById("viewpoli").style.display="block";
+    document.getElementById("viewhoverthreepoli").style.backgroundImage="url('../img/rp-hover.png')";
+    document.getElementById("viewhoverthreepoli").style.height="500px";
+
+}
+function displayvideothreeheros(){
+    document.getElementById("viewhoverheros").style.backgroundColor="";
+    document.getElementById("viewhoverheros").style.height="450px";
+    document.getElementById("viewhovertwoheros").style.backgroundColor="";
+    document.getElementById("viewhovertwoheros").style.height="450px";
+    document.getElementById("viewheros").style.display="block";
+    document.getElementById("viewhoverthreeheros").style.backgroundImage="url('../img/rp-hover.png')";
+    document.getElementById("viewhoverthreeheros").style.height="500px";
+
+}
+function displayvideothreefriends(){
+    document.getElementById("viewhoverfriends").style.backgroundColor="";
+    document.getElementById("viewhoverfriends").style.height="450px";
+    document.getElementById("viewhovertwofriends").style.backgroundColor="";
+    document.getElementById("viewhovertwofriends").style.height="450px";
+    document.getElementById("viewfriends").style.display="block";
+    document.getElementById("viewhoverthreefriends").style.backgroundImage="url('../img/rp-hover.png')";
+    document.getElementById("viewhoverthreefriends").style.height="500px";
+
+}
+function displaycomentar(){
+document.getElementById("viewtwo").style.display="block";
+document.getElementById("view").style.display="none";
+document.getElementById("viewhover").style.backgroundImage="url('../img/rp-hover.png')";
+document.getElementById("viewhover").style.height="500px";
+}
+
+function displaycomentarnew(){
+    document.getElementById("viewtwonew").style.display="block";
+    document.getElementById("viewnews").style.display="none";
+    document.getElementById("viewhover").style.backgroundImage="url('../img/rp-hover.png')";
+    document.getElementById("viewhover").style.height="500px";
+}
+function displaycomentargomelo(){
+    document.getElementById("viewtwogomelo").style.display="block";
+    document.getElementById("viewgomelo").style.display="none";
+    document.getElementById("viewhover").style.backgroundImage="url('../img/rp-hover.png')";
+    document.getElementById("viewhover").style.height="500px";
+}
+function displaycomentarpoli(){
+    document.getElementById("viewtwopoli").style.display="block";
+    document.getElementById("viewpoli").style.display="none";
+    document.getElementById("viewhover").style.backgroundImage="url('../img/rp-hover.png')";
+    document.getElementById("viewhover").style.height="500px";
+}
+function displaycomentarheros(){
+    document.getElementById("viewtwoheros").style.display="block";
+    document.getElementById("viewheros").style.display="none";
+    document.getElementById("viewhover").style.backgroundImage="url('../img/rp-hover.png')";
+    document.getElementById("viewhover").style.height="500px";
+}
+function displaycomentarfriends(){
+    document.getElementById("viewtwofriends").style.display="block";
+    document.getElementById("viewfriends").style.display="none";
+    document.getElementById("viewhover").style.backgroundImage="url('../img/rp-hover.png')";
+    document.getElementById("viewhover").style.height="500px";
+}
+function displaydescripcion(){
+    document.getElementById("viewtwo").style.display="none";
+    document.getElementById("view").style.display="block";
+    document.getElementById("viewhover").style.backgroundImage="url('../img/rp-hover.png')";
+    document.getElementById("viewhover").style.height="500px";
+      
+ }
+ function displaydescripcionnew(){
+    document.getElementById("viewtwonew").style.display="none";
+    document.getElementById("viewnews").style.display="block";
+    document.getElementById("viewhover").style.backgroundImage="url('../img/rp-hover.png')";
+    document.getElementById("viewhover").style.height="500px";
+      
+ }
+ function displaydescripciongomelo(){
+    document.getElementById("viewtwogomelo").style.display="none";
+    document.getElementById("viewgomelo").style.display="block";
+    document.getElementById("viewhover").style.backgroundImage="url('../img/rp-hover.png')";
+    document.getElementById("viewhover").style.height="500px";
+      
+ }
+ function displaydescripcionpoli(){
+    document.getElementById("viewtwopoli").style.display="none";
+    document.getElementById("viewpoli").style.display="block";
+    document.getElementById("viewhover").style.backgroundImage="url('../img/rp-hover.png')";
+    document.getElementById("viewhover").style.height="500px";
+      
+ }
+ function displaydescripcionheros(){
+    document.getElementById("viewtwoheros").style.display="none";
+    document.getElementById("viewheros").style.display="block";
+    document.getElementById("viewhover").style.backgroundImage="url('../img/rp-hover.png')";
+    document.getElementById("viewhover").style.height="500px";
+      
+ }
+ function displaydescripcionfriends(){
+    document.getElementById("viewtwofriends").style.display="none";
+    document.getElementById("viewfriends").style.display="block";
+    document.getElementById("viewhover").style.backgroundImage="url('../img/rp-hover.png')";
+    document.getElementById("viewhover").style.height="500px";
+      
+ }
+   
+    function displayreproducir(){
+     document.getElementById("reproducir").style.display="block";  
+    }
+    function regresar(){
+    document.getElementById("reproducir").style.display="none";  
+    }
+    function regresartwo(){
+    document.getElementById("comentar").style.display="none";
+    document.getElementById("bodyrep").style.opacity = "1";
+    }
+    function comentar(){
+    document.getElementById("bodyrep").style.display="block";
+    document.getElementById("bodyrep").style.opacity = "0.1";
+    document.getElementById("comentar").style.display="block";
+    }
+  
+ 
+
+
+    //*video*/
+    var mivideo= document.getElementById("video1");
+    function playPause(){
+
+        if(mivideo.paused)
+        (mivideo.play());
+        else
+        mivideo.pause();
+    }
+    function stop(){
+    mivideo.pause();
+    mivideo.currentTime=0;
+    }
+    function skip(value){
+ mivideo.currentTime+=value;
+
+    }
+
+
+
---
+++ js/indexx.js
@@ -0,0 +1,7 @@
+function displaydescripcionfriends(){
+    document.getElementById("viewtwofriends").style.display="none";
+    document.getElementById("viewfriends").style.display="block";
+    document.getElementById("viewhover").style.backgroundImage="url('../img/rp-hover.png')";
+    document.getElementById("viewhover").style.height="500px";
+      
+ }
