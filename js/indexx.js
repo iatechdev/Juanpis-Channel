809d6ae11928169d2acfd6f3e9cb3ee256a5cad4
iatechdev <Mauricio.roldan@iatech.com.co>
2020-08-20 22:41:12 -0500

    change

--- js/indexx.js
+++
@@ -1,7 +0,0 @@
-function displaydescripcionfriends(){
-    document.getElementById("viewtwofriends").style.display="none";
-    document.getElementById("viewfriends").style.display="block";
-    document.getElementById("viewhover").style.backgroundImage="url('../img/rp-hover.png')";
-    document.getElementById("viewhover").style.height="500px";
-      
- }
